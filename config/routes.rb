eccd689b7b0d169adaade561716fea6cba8b4808
Jindrich Vimr <jindrich.vimr@firma.seznam.cz>
2015-07-08 09:43:25 +0200

    devise login implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   
+
+  resources :home
+
+  resources :setup
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
